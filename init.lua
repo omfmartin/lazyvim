1f33adbee0943b6d5794c6fc01d4e9bc55e8770d
Olivier Martin <oliviermfmartin@tutanota.com>
2024-12-10 22:41:47 +0100

    python tools

--- init.lua
+++ init.lua
@@ -16,6 +16,35 @@ require("telekasten").setup({
   },
 })
 
+-- Mason
+require("mason").setup()
+require("mason-lspconfig").setup({
+  -- Install these LSPs automatically
+  ensure_installed = {
+    "bashls",
+    "cssls",
+    "html",
+    "gradle_ls",
+    "groovyls",
+    "lua_ls",
+    "jsonls",
+    "lemminx",
+    "marksman",
+    "quick_lint_js",
+    "yamlls",
+    "pyright",
+  },
+})
+require("mason-tool-installer").setup({
+  -- Install these linters, for
+  "black",
+  "debugpy",
+  "flake8",
+  "isort",
+  "mypy",
+  "pylint",
+})
+
 -- Markdown todo
 require("scripts.todo")
 
---
+++ lua/plugins/nvim-dap-python.lua
@@ -0,0 +1,9 @@
+return {
+  -- https://github.com/mfussenegger/nvim-dap-python
+  "mfussenegger/nvim-dap-python",
+  ft = "python",
+  dependencies = {
+    -- https://github.com/mfussenegger/nvim-dap
+    "mfussenegger/nvim-dap",
+  },
+}
