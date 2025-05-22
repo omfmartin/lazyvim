2b57b310ca1e49e3a8fba5ab70508161536b92a2
Olivier Martin <olivier.martin@emeal.nttdata.com>
2025-05-22 21:48:14 +0200

    blink cmp fix

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,6 +4,14 @@ return {
     opts = {
       keymap = {
         preset = "default",
+        ["<CR>"] = { "accept", "fallback" }, -- enable enter to accept
+      },
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+          },
+        },
       },
     },
   },
